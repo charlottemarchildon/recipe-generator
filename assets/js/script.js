a41886cbabf735ccc0102bf714b9ca85609d26b4
chvrlctte <200540177@student.georgianc.on.ca>
2023-11-08 20:47:39 -0500

    Started Javascript functionality

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("login-box").addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var username = document.getElementById("username-field").value;
+    var password = document.getElementById("password-field").value;
+})
