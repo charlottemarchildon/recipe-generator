9d109c3270e712c7cb5b92173ebfad780defb553
Karina Li <karina.yukting.li@gmail.com>
2023-11-09 21:09:16 -0500

    Added submit event listener and submit function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 // Get reference to form
+var form = document.getElementById("recipe-form");
+
 // Add submit event listener
-// Get form data
-// Send form data to server
-// Show success message
+form.addEventListener("submit", (e) => {
+  // Prevent default form submission
+  e.preventDefault();
+
+  var recipeName = document.getElementById("recipe-name").value;
+  console.log(recipeName);
+  var recipeIngredients = document.getElementById("recipe-ingredients").value;
+  console.log(recipeIngredients);
+  var recipeInstructions = document.getElementById("recipe-instructions").value;
+  console.log(recipeInstructions);
+});
+
+//Get form data
+//Send form data to server
+//Show sucess message
