7082b18d938a1fbe6620e0b6a16d7bedb68c6d05
Tiff Chan <tiffchan@Tiffanys-MacBook-Air.local>
2023-11-13 21:21:29 -0500

    Removed uncessary code

--- recipe-script.js
+++ recipe-script.js
@@ -1,17 +1,11 @@
 var searchInput = "French Toast"
 
 async function fetchData() {
-    // const url = `https://tasty.p.rapidapi.com/recipes/list?from=0&size=6&tags=${searchInput}`;
-    // const url = `https://tasty.p.rapidapi.com/recipes/get-more-info?id=8138`;
-    // const url = 'https://generate-a-recipe-based-on-an-ingredient.p.rapidapi.com/generate_recipe?ingredient=chicken';
+
     const url = 'https://food-recipes-with-images.p.rapidapi.com/?q=chicken%20soup';
     const options = {
         method: 'GET',
         headers: {
-            // 'X-RapidAPI-Key': 'c7274ee214msh7c69092222f1054p1d1942jsn85ff4436e42b',
-            // 'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-            // 'X-RapidAPI-Key': '74c3277fc3msh2b0e661bb1bd376p14e8c4jsn45af05e0e755',
-            // 'X-RapidAPI-Host': 'generate-a-recipe-based-on-an-ingredient.p.rapidapi.com'
             'X-RapidAPI-Key': '74c3277fc3msh2b0e661bb1bd376p14e8c4jsn45af05e0e755',
             'X-RapidAPI-Host': 'food-recipes-with-images.p.rapidapi.com'
         }
@@ -32,7 +26,7 @@ async function fetchData() {
         $(`#food-name`).text(foodName);
 
         // Ingredients Used
-        var ingredientList = JSON.stringify(data.d[0].Ingredients);
+        var ingredientList = JSON.stringify(data.d[3].Ingredients);
         var parsedList = ingredientList.split(`,"`);
         console.log(ingredientList);
         for (var i = 0; i < parsedList.length; i++) {
