a769d6349821864a4d9fa61d30f38cde3aa6f8ad
chvrlctte <200540177@student.georgianc.on.ca>
2023-11-16 17:10:43 -0500

    Added const for Remember User function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,11 @@ let loginWindow = new bootstrap.Modal(document.getElementById('login'));
 function login() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
+    const rememberUser = document.getElementById('rememberUser');
 
     loginWindow.show();
+
+    if (rememberUser) {
+        localStorage.setItem('username', username);
+    }
 }
