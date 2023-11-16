bd6efe452ee221c90028005c50db5911de7a470e
chvrlctte <200540177@student.georgianc.on.ca>
2023-11-16 16:36:49 -0500

    Added ability to show login window on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
+let loginWindow = new bootstrap.Modal(document.getElementById('login'));
+
 function login() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
+
+    loginWindow.show();
 }
